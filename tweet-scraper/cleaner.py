a83d1665b3d97380239840e33b478e78c79a77f4
hardik singh <realhardik18@gmail.com>
2025-04-12 18:41:49 +0530

    added data

---
+++ tweet-scraper/cleaner.py
@@ -0,0 +1,28 @@
+import json
+from datetime import datetime
+
+with open ("tweets.json", "r") as file:
+    tweets = json.load(file)
+
+def pretty_date(date_string):        
+    dt = datetime.strptime(date_string, "%a %b %d %H:%M:%S %z %Y")        
+    def ordinal(n):
+        return f"{n}{'th' if 11 <= n % 100 <= 13 else {1:'st', 2:'nd', 3:'rd'}.get(n % 10, 'th')}"        
+    return f"{ordinal(dt.day)} {dt.strftime('%B %Y')}"
+
+data=[]
+
+for tweet in tweets:
+    obj={
+        "profile_img": tweet['user']['legacy']['profile_image_url_https'],
+        "username": tweet['user']['legacy']['screen_name'],        
+        "created_at": pretty_date(tweet['tweet']['created_at']),
+        "likes": tweet['tweet']['favorite_count'],
+        "bookmarks": tweet['tweet']['bookmark_count'],
+        "reposts": tweet['tweet']['retweet_count'],
+        "content": tweet['tweet']['full_text'],
+        "tweet_url" : "https://x.com/"+tweet['user']['legacy']['screen_name']+"/status/"+tweet['tweet']['conversation_id_str'],
+    }    
+    data.append(obj)
+with open("tweets_clean.json", "w") as file:
+    json.dump(data, file, indent=4)
---
+++ tweet-scraper/main.py
@@ -0,0 +1,55 @@
+import json
+import os
+import time
+from dotenv import load_dotenv
+import requests
+# Replace with your RapidAPI key
+RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
+API_URL = "https://twitter241.p.rapidapi.com/tweet"
+
+HEADERS = {
+    "X-RapidAPI-Key": RAPIDAPI_KEY,
+    "X-RapidAPI-Host": "twitter241.p.rapidapi.com"
+}
+
+def fetch_tweet_details(tweet_id):
+    try:
+        response = requests.get(f"{API_URL}?pid={tweet_id}", headers=HEADERS)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print(f"Error fetching tweet {tweet_id}: {e}")
+        return None
+
+def main():
+    input_file = "batch1.txt"
+    output_file = "tweets.json"
+
+    if not os.path.exists(input_file):
+        print(f"Error: {input_file} not found.")
+        return
+
+    with open(input_file, "r") as file:
+        tweet_links = [line.strip() for line in file if line.strip()]
+
+    tweet_ids = [link.split('/')[-1] for link in tweet_links]
+
+    tweet_details = []
+    total_tweets = len(tweet_ids)
+    for index, tweet_id in enumerate(tweet_ids, start=1):
+        try:
+            details = fetch_tweet_details(tweet_id)
+            if details:
+                tweet_details.append(details)
+            time.sleep(2)  # Add a 2-second delay between requests
+            print(f"Progress: {index}/{total_tweets} tweets processed.")
+        except Exception as e:
+            print(f"Failed to fetch details for {tweet_id}: {e}")
+
+    with open(output_file, "w") as file:
+        json.dump(tweet_details, file, indent=4)
+
+    print(f"Tweet details saved to {output_file}")
+
+if __name__ == "__main__":
+    main()
